37e9729721482a23b91a178a1275e7346b2cbba5
Flcwl <flcwl@vip.qq.com>
2021-06-01 23:36:38 +0800

    feat: solved#876#206

--- src/leetcode训练/09.Bit-O/461.hamming-distance.js
+++ src/leetcode训练/09.Bit-O/461.hamming-distance.js
@@ -11,6 +11,7 @@
  * @return {number}
  */
 var hammingDistance = function(x, y) {
+  // 异或 得出不同 bit 位合并后的值，然后求出 1 的个数，即差异结果
   let t = x ^ y
   let ret = 0
 
@@ -23,5 +24,8 @@ var hammingDistance = function(x, y) {
   }
 
   return ret
+
+  // 利用 toString 转二进制
+  // return (x ^ y).toString(2).split('').filter((c) => c === '1').length
 };
 // @lc code=end
