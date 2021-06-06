28cb4e6cfeeee5b74a3b2728e9ac0549dba07a36
Flcwl <flcwl@vip.qq.com>
2021-06-06 13:00:36 +0800

    feat: solved#150

---
+++ src/leetcode训练/224.basic-calculator.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=224 lang=javascript
+ *
+ * [224] Basic Calculator
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+  const arr = s.split('')
+
+  for (let i = 0; i < arr.length; ++i) {
+
+  }
+};
+// @lc code=end
