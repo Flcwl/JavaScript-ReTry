44e4c98ef2894f7d24822a2500a4d780fa6881e4
Flcwl <flcwl@vip.qq.com>
2021-06-26 12:15:45 +0800

    feat: solved#895

--- src/data-structures/queue/queue.js
+++ src/data-structures/queue/queue.js
@@ -1,4 +1,5 @@
 var MyQueue = function () {
+  // 重构：使用链表实现
   this.list = [];
   this.start = 0;
   this.end = 0;
@@ -6,12 +7,6 @@ var MyQueue = function () {
 
 MyQueue.prototype.enqueue = function (x) {
   this.list[this.end++] = x;
-};
-MyQueue.prototype.top = function (x) {
-  return this.list[this.start];
-};
-MyQueue.prototype.empty = function (x) {
-  return this.start === this.end;
 };
 MyQueue.prototype.dequeue = function (x) {
   if (this.empty()) {
@@ -23,4 +18,13 @@ MyQueue.prototype.dequeue = function (x) {
   this.list[this.start] = undefined;
   this.start++;
   return ret;
+};
+MyQueue.prototype.top = function () {
+  return this.list[this.start];
+};
+MyQueue.prototype.tail = function () {
+  return this.list[this.end - 1];
+};
+MyQueue.prototype.empty = function (x) {
+  return this.start === this.end;
 };
