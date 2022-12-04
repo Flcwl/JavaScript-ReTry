a11e06cbdbac8fe6e87fa4e497c0545f24a60c5b
Flcwl <flcwl@vip.qq.com>
2022-12-04 19:08:50 +0800

    feat: solve problem for 28

---
+++ src/leetcode训练/00.Basic-Grammar/28.implement-str-str.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  for (let i = 0; ; i++) {
+    for (let j = 0; ; j++) {
+      if (j === needle.length) return i;
+      if (i + j >= haystack.length) return -1;
+      // 不一定在首位
+      if (needle[j] !== haystack[i + j]) break;
+    }
+  }
+};
